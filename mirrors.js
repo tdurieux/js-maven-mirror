82a5b1f64044521a02e1026ee4754655e3e5becb
Thomas Durieux <durieuxthomas@hotmail.com>
2021-03-31 14:27:25 +0200

    Add sonatype mirror

--- mirrors.js
+++ mirrors.js
@@ -3,4 +3,5 @@ module.exports = [
   "https://repo1.maven.apache.org/maven2/",
   "https://central.maven.org/maven2/",
   "https://maven-central-eu.storage-download.googleapis.com/maven2/",
+  "https://oss.sonatype.org/content/groups/public/",
 ];
